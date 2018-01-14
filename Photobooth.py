a889c4b718accd5acc8141643bddec50c319821e
Jeremy Shaw <jeremy@n-heptane.com>
2018-01-14 19:45:44 +0000

    updates to Photobooth.py

--- Photobooth.py
+++ Photobooth.py
@@ -16,9 +16,21 @@ class Mode(Enum):
     PREVIEW   = 2
     REVIEW    = 3
 
+monitor_res = (640,480)
 res = (2592,1944)
 camera = PiCamera(resolution=res, framerate=15)
+print("max resolution = " + str(camera.MAX_RESOLUTION))
 camera.vflip = True
+camera.hflip = True
+#camera.awb_mode = 'off'
+#camera.awb_gains=(1.0, 2.0)
+camera.exposure_mode = 'auto'
+#camera.shutter_speed = 1000000
+g = camera.awb_gains
+print ('awb_gains = '+ str(g))
+##camera.awb_mode = 'off'
+##camera.awb_gains = g
+# camera.image_effect='sketch';
 eventQ = Queue(maxsize=0)
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -67,10 +79,29 @@ def handle_event(q):
         ))
     one.paste(one_png, (0, 0))        
 
-    review = Image.new('RGB', res);
+    take_photo_png = Image.open("take-photo.png")
+    take_photo = Image.new('RGB',
+        (((take_photo_png.size[0] + 31) // 32) * 32,
+         ((take_photo_png.size[1] + 15) // 16) * 16,
+        ))
+    take_photo.paste(take_photo_png, (0, 0))        
+    take_photo_o = None;
+    
+    approve_reject_png = Image.open("approve-reject.png")
+    approve_reject = Image.new('RGB',
+        (((approve_reject_png.size[0] + 31) // 32) * 32,
+         ((approve_reject_png.size[1] + 15) // 16) * 16,
+        ))
+    approve_reject.paste(approve_reject_png, (0, 0))        
+    approve_reject_o = None;
+
+    
+#    review = Image.new('RGB', res);
     review_o = None;
     filename = ""
 
+
+    
     slideshow_files = [f for f in os.listdir("approve") if f.endswith('.jpg')]
     slideshow_index = 0
     slideshow_o = None;
@@ -84,6 +115,12 @@ def handle_event(q):
                 camera.remove_overlay(slideshow_o)
                 slideshow_o = None;
 
+            take_photo_o = camera.add_overlay(take_photo.tobytes(), size=take_photo.size)
+            take_photo_o.alpha=128
+            take_photo_o.layer=3
+            take_photo_o.fullscreen = False;
+            take_photo_o.window=((monitor_res[0]-512)//2, monitor_res[1]-96,512,96)
+                
             camera.start_preview(fullscreen=True)
             
         if mode == Mode.SLIDESHOW and msg.payload == b'next':
@@ -100,6 +137,9 @@ def handle_event(q):
             
         elif mode == Mode.PREVIEW and msg.payload == b'shutter':
             print("SHUTTER");
+            if (take_photo_o != None):
+                camera.remove_overlay(take_photo_o)
+                take_photo_o = None
             o = camera.add_overlay(three.tobytes(), size=three.size)
             o.alpha = 32
             o.layer = 3
@@ -119,24 +159,40 @@ def handle_event(q):
             print(filename)
             camera.capture(filename);
             camera.stop_preview();
+            print("REVIEW")
+            
             review_jpg = Image.open(filename)
             review_o = camera.add_overlay(review_jpg.tobytes(), size=review_jpg.size)
             review_o.alpha=255
-            review_o.layer=3
-            print("REVIEW")
+            review_o.layer=1
+
+            approve_reject_o = camera.add_overlay(approve_reject.tobytes(), size=approve_reject.size)
+            approve_reject_o.alpha=128
+            approve_reject_o.layer=2
+            approve_reject_o.fullscreen = False;
+            approve_reject_o.window=((monitor_res[0]-512)//2, monitor_res[1]-96,512,96)
             mode = Mode.REVIEW
+            
         elif mode == Mode.REVIEW and msg.payload == b'approve':
-            print("SLIDESHOW")
+            print("APPROVED")
+            if approve_reject_o != None:
+                camera.remove_overlay(approve_reject_o)
+                approve_reject_o = None
             camera.remove_overlay(review_o);
             os.rename(filename, "approve/"+filename);
             slideshow_files.append(filename);
             filename = ""
+            print("SLIDESHOW")
             mode = Mode.SLIDESHOW
         elif mode == Mode.REVIEW and msg.payload == b'reject':
-            print("SLIDESHOW")
+            print("REJECTED")
+            if approve_reject_o != None:
+                camera.remove_overlay(approve_reject_o)
+                approve_reject_o = None
             camera.remove_overlay(review_o);            
             os.rename(filename, "reject/"+filename);
             filename = ""
+            print("SLIDESHOW")            
             mode = Mode.SLIDESHOW
         else:
             print (mode, str(msg.payload))
